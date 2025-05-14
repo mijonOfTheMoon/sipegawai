83c4b24e5dac816a0189c4d878fa96bd6a4364eb
Hasbi Mizan <hasbimizan@student.ub.ac.id>
2025-05-14 09:57:35 +0700

    Fix database seeders

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Pegawai;
 use Illuminate\Database\Seeder;
-use Haruncpi\LaravelIdGenerator\IdGenerator;
+
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 use Spatie\Permission\Models\Permission;
@@ -27,7 +27,12 @@ class PegawaiSeeder extends Seeder
         $permissions = Permission::pluck('id', 'id')->all();
 
         for ($i = 0; $i <= 2; $i++) {
-            $id = IdGenerator::generate(['table' => 'pegawai', 'length' => 8, 'prefix' => date('ym')]);
+            // Custom ID generator compatible with existing format
+            $prefix = date('ym');
+            $lastId = DB::table('pegawai')->orderBy('id', 'desc')->value('id');
+            $lastNumber = $lastId ? intval(substr($lastId, 4)) : 0;
+            $newNumber = $lastNumber + 1;
+            $id = $prefix . str_pad($newNumber, 4, '0', STR_PAD_LEFT);
             $jkp = $faker->numberBetween(0, 1);
             $jbtn = $faker->numberBetween(1, 9);
             $relg = $faker->numberBetween(0, 5);
@@ -60,76 +65,5 @@ class PegawaiSeeder extends Seeder
 
             $pegawai->assignRole([$role[$i]]);
         }
-
-        // for ($i = 0; $i <= 2; $i++) {
-        //     $id = IdGenerator::generate(['table' => 'pegawai', 'length' => 8, 'prefix' => date('ym')]);
-        //     $jkp = $faker->numberBetween(0, 1);
-        //     $jbtn = $faker->numberBetween(1, 9);
-        //     $relg = $faker->numberBetween(0, 5);
-        //     $role = [3, 3, 2];
-        //     $email = ['haniffauzihakim4049@gmail.com', 'haniffauzihakim5059@gmail.com', 'mr.expendables25@gmail.com'];
-        //     $dvs = $faker->numberBetween(2, 4);
-
-        //     $pegawai = Pegawai::create([
-        //         'id' => $id,
-        //         'id_role' => $role[$i],
-        //         'nik' => $faker->nik(),
-        //         'nama' => $faker->name,
-        //         'jk' => $jk[$jkp],
-        //         'agama' => $agama[$relg],
-        //         'tempat_lahir' => $faker->city,
-        //         'tgl_lahir' => $faker->date,
-        //         'alamat_ktp' => $faker->streetAddress,
-        //         'alamat_dom' => $faker->streetAddress,
-        //         'status' => $status[$jkp],
-        //         'jml_anak' => $relg,
-        //         'no_hp' => $faker->phoneNumber,
-        //         'email' => $email[$i],
-        //         'password' => bcrypt('123456'),
-        //         'tgl_masuk' => $faker->date,
-        //         'id_jabatan' => $jbtn,
-        //         'id_atasan' => NULL,
-        //         'id_divisi' => $dvs,
-        //         'path' => 'foto.jpg'
-        //     ]);
-
-        //     $pegawai->assignRole([$role[$i]]);
-        // }
-
-
-        // for ($i = 0; $i <= 100; $i++) {
-        //     $id = IdGenerator::generate(['table' => 'pegawai', 'length' => 8, 'prefix' => date('ym')]);
-        //     $jkp = $faker->numberBetween(0, 1);
-        //     $jbtn = $faker->numberBetween(1, 9);
-        //     $relg = $faker->numberBetween(0, 5);
-        //     $role = [3, 3, 2];
-        //     $email = ['haniffauzihakim4049@gmail.com', 'haniffauzihakim5059@gmail.com', 'mr.expendables25@gmail.com'];
-        //     $dvs = $faker->numberBetween(2, 4);
-
-        //     $pegawai = Pegawai::create([
-        //         'id' => $id,
-        //         'id_role' => $role[2],
-        //         'nik' => $faker->nik(),
-        //         'nama' => $faker->name,
-        //         'jk' => $jk[$jkp],
-        //         'agama' => $agama[$relg],
-        //         'tempat_lahir' => $faker->city,
-        //         'tgl_lahir' => $faker->date,
-        //         'alamat_ktp' => $faker->streetAddress,
-        //         'alamat_dom' => $faker->streetAddress,
-        //         'status' => $status[$jkp],
-        //         'jml_anak' => $relg,
-        //         'no_hp' => $faker->phoneNumber,
-        //         'email' => $faker->email,
-        //         'password' => bcrypt('123456'),
-        //         'tgl_masuk' => $faker->date,
-        //         'id_jabatan' => $jbtn,
-        //         'id_atasan' => NULL,
-        //         'id_divisi' => $dvs,
-        //         'path' => 'foto.jpg'
-        //     ]);
-
-        //     $pegawai->assignRole([$role[2]]);
-        // }
     }
 }
